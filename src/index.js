12862bf328e61186f946585f18b7911c732583d5
Aquib Master <aquibmaster@gmail.com>
2016-10-06 22:34:28 +1300

    Implement basic input validation and write tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-module.exports = () => {
-    // eslint-disable-next-line no-console
-    console.log('This is a placeholder :)');
+/*
+    tag-uri: A node implementation of RFC4151.
+*/
+'use strict';
+
+const validateInputs = (uri, date) => {
+    if(uri === undefined || typeof(uri) !== 'string') {
+        throw Error('Expected uri to be a string');
+    }
+
+    if(date === undefined || typeof(date) !== 'object') {
+        throw Error('Expected date to be a date');
+    }
 };
+
+function tag(uri, date = new Date()) {
+    validateInputs(uri, date);
+
+    // Do nothing.
+}
+
+module.exports = tag;

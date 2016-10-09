4ec20d2133eca3ec56c9f857b424e67970cba470
Aquib Master <aquibmaster@gmail.com>
2016-10-09 13:05:32 +1300

    Implement simple tagging. Fix version. Tests.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+import url from 'url';
+import moment from 'moment';
 import validateInputs from './util/validateInputs';
+
+function getDateString(date) {
+    return moment(date).format('YYYY-MM-DD');
+}
+
+function translateSlashes(pathname) {
+    const replaced = pathname.replace(/\//g, ':');
+    return replaced.length > 1 ? replaced : '';
+}
 
 function tag(uri, date = new Date()) {
     validateInputs(uri, date);
 
-    // Do nothing.
+    const parsedUrl = url.parse(uri);
+    return `tag:${parsedUrl.hostname},${getDateString(date)}${translateSlashes(parsedUrl.pathname)}`;
 }
 
 module.exports = tag;
--- test/index-spec.js
+++ test/index-spec.js
@@ -1,8 +1,34 @@
 import test from 'ava';
 import tag from '../lib';
 
+const neverForget = new Date(2016, 4, 28);
+
 test('should be able to instantiate without the date parameter', t => {
     t.notThrows(() => {
         tag('Hello!');
     });
+});
+
+test('should be able to convert simple domain names', t => {
+    const tagUri = tag('http://gmail.com', neverForget);
+
+    t.is(tagUri, 'tag:gmail.com,2016-05-28');
+});
+
+test('should be able to convert domains with trailing slashes', t => {
+    const tagUri = tag('http://gmail.com/', neverForget);
+    
+    t.is(tagUri, 'tag:gmail.com,2016-05-28');
+});
+
+test('should be able to convert subdomains', t => {
+    const tagUri = tag('http://mail.google.com/', neverForget);
+
+    t.is(tagUri, 'tag:mail.google.com,2016-05-28');
+});
+
+test('should be able to convert url\'s with paths', t => {
+    const tagUri = tag('http://mail.google.com/u/1', neverForget);
+
+    t.is(tagUri, 'tag:mail.google.com,2016-05-28:u:1');
 });
